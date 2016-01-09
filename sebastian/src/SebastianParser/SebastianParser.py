006f7a2a096be08b1d5bdcfdcebfad701265d07c
calpeyser <calpeyser@gmail.com>
2016-01-09 13:56:02 -0500

    Some parsing documentation

--- sebastian/src/SebastianParser/SebastianParser.py
+++ sebastian/src/SebastianParser/SebastianParser.py
@@ -5,6 +5,13 @@ from music21 import converter
 from music21 import stream
 
 class SebastianParser():
+    """
+    Parses a MusicXML file in to a chorale.  Performs validation to ensure that the chorale
+    can be analyzed by Sebastian.
+
+    Should be implemented with a group of "validator" functions, which check a condition on the
+    chorale.  Each validator is run, with a failed validator throwing a SebastianParsingException.
+    """
 
     def __init__(self, path):
         self.path = path
@@ -14,6 +21,11 @@ class SebastianParser():
         ]
 
     def parse_as_chorale(self):
+        """
+        Throws a SebastianParsingException if the chorale is malformed.
+
+        :return: A chorale instance.
+        """
         self.parsed_input = converter.parse(self.path)
         self.validate()
         return SebastianStructures.Chorale(self.parsed_input)
